7aec5caed474f3cb85c303c1a48504cd489256ac
scottie-housholder <scottie_housholder@taylor.edu>
2025-04-04 18:44:30 -0400

    Carts and such

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,21 @@
 class Product < ApplicationRecord
-  validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)\z}i,
-    message: "must be a URL for GIF, JPG, or PNG images."
-  }
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    validates :title, :description, :image_url, presence: true
+    validates :title, uniqueness: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :image_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|png)\z}i,
+      message: "must be a URL for GIF, JPG, or PNG images."
+    }
+
+    private
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort
+            end
+        end
 end
